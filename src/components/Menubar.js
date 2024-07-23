56fe684e3ac1ea82c03b5629822050875de93a30
Florakimm2 <florakimm2@gmail.com>
2024-07-24 02:16:34 +0900

    메뉴바 파일 생성 및 메인화면 초기화

---
+++ src/components/Menubar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Menubar.css';
+
+const Menubar = () => {
+  return (
+    <nav className="menubar">
+      <div className="menubar-logo">
+        <Link to="/">Healthcare Inc.</Link>
+      </div>
+      <ul className="menubar-menu">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menubar;
