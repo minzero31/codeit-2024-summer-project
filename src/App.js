88798c71973ef4f9958a7e788c5cecf9f0d0d0a1
Florakimm2 <florakimm2@gmail.com>
2024-07-25 01:33:04 +0900

    메뉴바 아이콘 설정, 디자인(비율) 조정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Home from './components/Home';
-//import Contact from './components/Contact';
-//<Route path="/contact" element={<Contact />} />
-import Menubar from './components/Menubar';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Menubar from './components/Menubar';
+import Home from './components/Home';
 import Schedule from './components/Schedule';
-import Professors from './components/Professors';
-import Test from './components/Test';
 import Find from './components/Find';
+import Professors from './components/Professors';
 import Chat from './components/Chat';
+import Test from './components/Test';
 
 function App() {
   return (
--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -6,15 +6,15 @@ const Menubar = () => {
   return (
     <nav className="menubar">
       <div className="menubar-logo">
-        <Link to="/">ㅁ</Link>
+        <Link to="/"><img src="images/menu4_icon.png" alt="Menu" /></Link>
       </div>
       <ul className="menubar-menu">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/schedule">Schedule</Link></li>
-        <li><Link to="/find">Find</Link></li>
-        <li><Link to="/chat">Chat</Link></li>
-        <li><Link to="/professors">Professors</Link></li>
-        <li><Link to="/test">Test</Link></li>
+        <li><Link to="/"><img src="images/home2_icon.png" alt="Home" /></Link></li>
+        <li><Link to="/schedule"><img src="images/cal2_icon.png" alt="Schedule" /></Link></li>
+        <li><Link to="/find"><img src="images/map_icon.png" alt="Find" /></Link></li>
+        <li><Link to="/chat"><img src="images/chat_icon.png" alt="Chat" /></Link></li>
+        <li><Link to="/professors"><img src="images/doctor2_icon.png" alt="Professors" /></Link></li>
+        <li><Link to="/test"><img src="images/list_icon.png" alt="Test" /></Link></li>
       </ul>
     </nav>
   );
