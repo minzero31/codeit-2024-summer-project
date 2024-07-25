b68ff9fa9054f69e1a19dccf72cc135d0a4bda5c
Florakimm2 <florakimm2@gmail.com>
2024-07-25 18:39:32 +0900

    block4 수정

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,25 +28,36 @@ const Home = () => {
             </div>
             <div className='para3'>
                 <section className='block4'>
-                    <h2>오늘 어떤 약을<br/>복용하셨습니까?</h2>
+                    <h2>오늘 어떤 약💊을<br/>복용하셨습니까?</h2>
                     <fieldset>
-                        <legend></legend>
-                        <div>
+                        <div id='option'>
                             <input type='checkbox' id='med1' name='복용약 종류' value='1번 약(입력되는 값?)' />
-                            <label for='med1'>약1번</label>
+                            <label for='med1'>약1</label>
                         </div>
-                        <div>
+                        <div id='option'>
                             <input type='checkbox' id='med2' name='복용약 종류' value='2번 약(입력되는 값?)' />
-                            <label for='med2'>약2번</label>
+                            <label for='med2'>약2</label>
                         </div>
-                        <div>
+                        <div id='option'>
                             <input type='checkbox' id='med3' name='복용약 종류' value='3번 약(입력되는 값?)' />
-                            <label for='med3'>약3번</label>
+                            <label for='med3'>약3</label>
+                        </div>
+                        <div id='option'>
+                            <input type='checkbox' id='med4' name='복용약 종류' value='4번 약(입력되는 값?)' />
+                            <label for='med4'>약4</label>
+                        </div>
+                        <div id='option'>
+                            <input type='checkbox' id='med5' name='복용약 종류' value='5번 약(입력되는 값?)' />
+                            <label for='med5'>약5</label>
+                        </div>
+                        <div id='option'>
+                            <input type='checkbox' id='med6' name='복용약 종류' value='6번 약(입력되는 값?)' />
+                            <label for='med6'>약6</label>
                         </div>
                     </fieldset>
-                    <button>+ 복용 루틴 추가하기</button>
+                    <button id='addMed'>+   복용 루틴 추가하기</button>
                     <br />
-                    <button>저장하기</button>
+                    <button id="saveMed">저장하기</button>
                 </section>
             </div>
         </main>
