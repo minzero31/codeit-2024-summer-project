84586f8b2f169be12276cf9dd520daa7f5b62c23
Florakimm2 <florakimm2@gmail.com>
2024-08-01 02:17:32 +0900

    메인창 block1 제외 프앤 마무리, schedule창 블록들 구성 및 blck2 디자인

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import './Schedule.css'
 
 const Schedule = () => {
   return (
-    <div>
-      <h1>Schedule</h1>
-      <p>진단 기록 및 약 복용 기록</p>
-    </div>
+    <>
+      <div>
+        <h1 id='schh1'>Schedule 진단 기록 및 약 복용 기록</h1>
+      </div>
+      <main className='schmain'>
+        <div className='par0'>
+          <p> </p>
+        </div>
+        <div className='par1'>
+          <section className='blck1'>
+            <h2 id='schh2'> </h2>
+            <img src='images/sampleimage_cal.avif' alt='달력샘플이미지'/>
+          </section>
+        </div>
+        <div className='par2'>
+          <section className='blck2'>
+            <h2 id='schh2'>진료 기록</h2>
+            <fieldset className='schsec1'>
+              <label>
+                병원명
+              </label>
+            </fieldset>
+            <fieldset className='schsec2'>
+              <label></label>
+            </fieldset>
+            <button id="schsavebtn">저장하기</button>
+
+          </section>
+        </div>
+      </main>
+    </>
   );
 };
 
