30aa13d194b104e0d8707fd1c608a978c30cfa2c
hyeonseo <ohs4784@gmail.com>
2024-08-18 23:28:22 +0900

    메뉴바 로그인페이지에서 숨기기, 메뉴바 이동 home으로 할 수 있게 수정

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import LoginRegister from "./components/LoginRegister/LoginRegister";
-import Main from "./components/Main/main.js";
-import Menubar from "./components/Menubar";
 import Home from "./components/Home";
 import Schedule from "./components/Schedule";
 import Find from "./components/Find";
 import Professors from "./components/Professors";
 import Chat from "./components/Chat";
 import Test from "./components/Test";
+import Menubar from "./components/Menubar";
+
+const AppContent = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/";
+
+  return (
+    <div className="App">
+      {/* LoginRegister 페이지가 아닌 경우에만 로고와 메뉴바를 표시 */}
+      {!isLoginPage && (
+        <>
+          <header className="topLogo">
+            <p>Healthcare Inc.(로고)</p>
+          </header>
+          <div className="Bar">
+            <Menubar />
+          </div>
+        </>
+      )}
+      <Routes>
+        <Route path="/" element={<LoginRegister />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/find" element={<Find />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/professors" element={<Professors />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="topLogo">
-          <p>Healthcare Inc.(로고)</p>
-        </header>
-        <div className="Bar">
-          <Menubar />
-          <Routes>
-            <Route path="/" element={<LoginRegister />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/find" element={<Find />} />
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/professors" element={<Professors />} />
-            <Route path="/test" element={<Test />} />
-          </Routes>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 }
--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Menubar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Menubar.css";
 
 const Menubar = () => {
   return (
     <nav className="menubar">
       <div className="menubar-logo">
-        <Link to="/"><img src="images/menu4_icon.png" alt="Menu" /></Link> 
+        <Link to="/">
+          <img src="images/menu4_icon.png" alt="Menu" />
+        </Link>
       </div>
       <ul className="menubar-menu">
-        <li><Link to="/"><img src="images/home2_icon.png" alt="Home" /></Link></li>
-        <li><Link to="/schedule"><img src="images/cal2_icon.png" alt="Schedule" /></Link></li>
-        <li><Link to="/find"><img src="images/map_icon.png" alt="Find" /></Link></li>
-        <li><Link to="/chat"><img src="images/chat_icon.png" alt="Chat" /></Link></li>
-        <li><Link to="/professors"><img src="images/doctor2_icon.png" alt="Professors" /></Link></li>
-        <li><Link to="/test"><img src="images/list_icon.png" alt="Test" /></Link></li>
+        <li>
+          <Link to="/home">
+            <img src="images/home2_icon.png" alt="Home" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/schedule">
+            <img src="images/cal2_icon.png" alt="Schedule" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/find">
+            <img src="images/map_icon.png" alt="Find" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/chat">
+            <img src="images/chat_icon.png" alt="Chat" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/professors">
+            <img src="images/doctor2_icon.png" alt="Professors" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/test">
+            <img src="images/list_icon.png" alt="Test" />
+          </Link>
+        </li>
       </ul>
     </nav>
   );
